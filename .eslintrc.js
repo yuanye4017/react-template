2aab2e8f172209c844867cb7442b7b1c951be940
v-yuanye <v-yuanyeu@guazi.com>
2020-04-10 17:01:49 +0800

    更改eslink规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,111 +1,228 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    //关闭换行符操作系统格式问题
+    "linebreak-style": [
+      "off",
+      "unix"
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    "quotes": [
+      "error",
+      "single"
     ],
-    "rules": {
-        "quotes": [2, "single"], //单引号
-        "no-console": 0, //不禁用console
-        "no-debugger": 2, //禁用debugger
-        "no-var": 0, //对var警告
-        "semi": 0, //不强制使用分号
-        "no-irregular-whitespace": 0, //不规则的空白不允许
-        "no-trailing-spaces": 1, //一行结束后面有空格就发出警告
-        "eol-last": 0, //文件以单一的换行符结束
-        "no-unused-vars": [2, {
-          "vars": "all",
-          "args": "after-used"
-        }], //不能有声明后未被使用的变量或参数
-        'no-multi-spaces': 2, // 不允许多个空格
-        'no-regex-spaces': 2, // 不允许在正则表达式中使用多个空格
-        "no-underscore-dangle": 0, //标识符不能以_开头或结尾
-        "no-alert": 2, //禁止使用alert confirm prompt
-        "no-lone-blocks": 0, //禁止不必要的嵌套块
-        "no-class-assign": 2, //禁止给类赋值
-        "no-cond-assign": 2, //禁止在条件表达式中使用赋值语句
-        "no-const-assign": 2, //禁止修改const声明的变量
-        "no-delete-var": 2, //不能对var声明的变量使用delete操作符
-        "no-dupe-keys": 2, //在创建对象字面量时不允许键重复
-        "no-duplicate-case": 2, //switch中的case标签不能重复
-        "no-dupe-args": 2, //函数参数不能重复
-        "no-empty": 2, //块语句中的内容不能为空
-        "no-func-assign": 2, //禁止重复的函数声明
-        "no-invalid-this": 0, //禁止无效的this，只能用在构造器，类，对象字面量
-        "no-redeclare": 2, //禁止重复声明变量
-        "no-spaced-func": 2, //函数调用时 函数名与()之间不能有空格
-        "no-this-before-super": 0, //在调用super()之前不能使用this或super
-        "no-undef": 2, //不能有未定义的变量
-        "no-use-before-define": 2, //未定义前不能使用
-        "camelcase": 0, //强制驼峰法命名
-        "jsx-quotes": [2, "prefer-double"], //强制在JSX属性（jsx-quotes）中一致使用双引号
-        "react/display-name": 0, //防止在React组件定义中丢失displayName
-        "react/forbid-prop-types": [2, {
-          "forbid": ["any"]
-        }], //禁止某些propTypes
-        "react/jsx-boolean-value": 2, //在JSX中强制布尔属性符号
-        "react/jsx-closing-bracket-location": 1, //在JSX中验证右括号位置
-        "react/jsx-curly-spacing": [2, {
-          "when": "never",
-          "children": true
-        }], //在JSX属性和表达式中加强或禁止大括号内的空格。
-        "react/jsx-indent-props": [2, 4], //验证JSX中的props缩进
-        "react/jsx-key": 2, //在数组或迭代器中验证JSX具有key属性
-        "react/jsx-max-props-per-line": [1, {
-          "maximum": 3
-        }], // 限制JSX中单行上的props的最大数量
-        "react/jsx-no-bind": 0, //JSX中不允许使用箭头函数和bind
-        "react/jsx-no-duplicate-props": 2, //防止在JSX中重复的props
-        "react/jsx-no-literals": 0, //防止使用未包装的JSX字符串
-        "react/jsx-no-undef": 1, //在JSX中禁止未声明的变量
-        "react/jsx-pascal-case": 0, //为用户定义的JSX组件强制使用PascalCase
-        "react/jsx-sort-props": 2, //强化props按字母排序
-        "react/jsx-uses-react": 1, //防止反应被错误地标记为未使用
-        "react/jsx-uses-vars": 2, //防止在JSX中使用的变量被错误地标记为未使用
-        "react/no-danger": 0, //防止使用危险的JSX属性
-        "react/no-did-mount-set-state": 0, //防止在componentDidMount中使用setState
-        "react/no-did-update-set-state": 1, //防止在componentDidUpdate中使用setState
-        "react/no-direct-mutation-state": 2, //防止this.state的直接变异
-        "react/no-multi-comp": 2, //防止每个文件有多个组件定义
-        "react/no-set-state": 0, //防止使用setState
-        "react/no-unknown-property": 2, //防止使用未知的DOM属性
-        "react/prefer-es6-class": 2, //为React组件强制执行ES5或ES6类
-        "react/prop-types": 0, //防止在React组件定义中丢失props验证
-        "react/react-in-jsx-scope": 2, //使用JSX时防止丢失React
-        "react/self-closing-comp": 0, //防止没有children的组件的额外结束标签
-        "react/sort-comp": 2, //强制组件方法顺序
-        "no-extra-boolean-cast": 0, //禁止不必要的bool转换
-        "react/no-array-index-key": 0, //防止在数组中遍历中使用数组key做索引
-        "react/no-deprecated": 1, //不使用弃用的方法
-        "react/jsx-equals-spacing": 2, //在JSX属性中强制或禁止等号周围的空格
-        "no-unreachable": 1, //不能有无法执行的代码
-        "comma-dangle": 2, //对象字面量项尾不能有逗号
-        "no-mixed-spaces-and-tabs": 0, //禁止混用tab和空格
-        "prefer-arrow-callback": 0, //比较喜欢箭头回调
-        "arrow-parens": 0, // 箭头函数用小括号括起来
-        'arrow-spacing': [2, { // 箭头函数中的箭头前后强制一致的间距
-          'before': true,
-          'after': true
-        }],
-        'comma-style': [2, 'last'],
-        'semi': [2, 'never'], // 禁止分号结尾
-        'no-param-reassign': 0 // 函数可最改形参
-    }
+    "semi": [
+      "error",
+      "always"
+    ],
+    // 关闭解构赋值报错
+    "react/destructuring-assignment": 0,
+    // 允许在 .js 和 .jsx 文件中使用  jsx
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }],
+    // jsx > 紧跟着属性
+    "react/jsx-closing-bracket-location": [1, "after-props"],
+    // 不区分是否是 无状态组件
+    "react/prefer-stateless-function": 0,
+    // prop-types忽略children属性
+    "react/prop-types": [1, {
+      ignore: ["children"]
+    }],
+    // 关闭禁止prop-types类型
+    "react/forbid-prop-types": 0,
+    // 关闭default-props检查
+    "react/require-default-props": 0,
+    'accessor-pairs': 2,
+    'arrow-spacing': [2, {
+      'before': true,
+      'after': true
+    }],
+    'block-spacing': [2, 'always'],
+    'brace-style': [2, '1tbs', {
+      'allowSingleLine': true
+    }],
+    'camelcase': [0, {
+      'properties': 'always'
+    }],
+    'comma-dangle': [2, 'never'],
+    'comma-spacing': [2, {
+      'before': false,
+      'after': true
+    }],
+    'comma-style': [2, 'last'],
+    'constructor-super': 2,
+    'curly': [2, 'multi-line'],
+    'dot-location': [2, 'property'],
+    'eol-last': 2,
+    'eqeqeq': ["error", "always", {
+      "null": "ignore"
+    }],
+    'generator-star-spacing': [2, {
+      'before': true,
+      'after': true
+    }],
+    'handle-callback-err': [2, '^(err|error)$'],
+    'indent': [2, 2, {
+      'SwitchCase': 1
+    }],
+    'jsx-quotes': [2, 'prefer-single'],
+    'key-spacing': [2, {
+      'beforeColon': false,
+      'afterColon': true
+    }],
+    'keyword-spacing': [2, {
+      'before': true,
+      'after': true
+    }],
+    'new-cap': [2, {
+      'newIsCap': true,
+      'capIsNew': false
+    }],
+    'new-parens': 2,
+    'no-array-constructor': 2,
+    'no-caller': 2,
+    'no-console': 'off',
+    'no-class-assign': 2,
+    'no-cond-assign': 2,
+    'no-const-assign': 2,
+    'no-control-regex': 0,
+    'no-delete-var': 2,
+    'no-dupe-args': 2,
+    'no-dupe-class-members': 2,
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    'no-empty-character-class': 2,
+    'no-empty-pattern': 2,
+    'no-eval': 2,
+    'no-ex-assign': 2,
+    'no-extend-native': 2,
+    'no-extra-bind': 2,
+    'no-extra-boolean-cast': 2,
+    'no-extra-parens': [2, 'functions'],
+    'no-fallthrough': 2,
+    'no-floating-decimal': 2,
+    'no-func-assign': 2,
+    'no-implied-eval': 2,
+    'no-inner-declarations': [2, 'functions'],
+    'no-invalid-regexp': 2,
+    'no-irregular-whitespace': 2,
+    'no-iterator': 2,
+    'no-label-var': 2,
+    'no-labels': [2, {
+      'allowLoop': false,
+      'allowSwitch': false
+    }],
+    'no-lone-blocks': 2,
+    'no-mixed-spaces-and-tabs': 2,
+    'no-multi-spaces': 2,
+    'no-multi-str': 2,
+    'no-multiple-empty-lines': [2, {
+      'max': 1
+    }],
+    'no-native-reassign': 2,
+    'no-negated-in-lhs': 2,
+    'no-new-object': 2,
+    'no-new-require': 2,
+    'no-new-symbol': 2,
+    'no-new-wrappers': 2,
+    'no-obj-calls': 2,
+    'no-octal': 2,
+    'no-octal-escape': 2,
+    'no-path-concat': 2,
+    'no-proto': 2,
+    'no-redeclare': 2,
+    'no-regex-spaces': 2,
+    'no-return-assign': [2, 'except-parens'],
+    'no-self-assign': 2,
+    'no-self-compare': 2,
+    'no-sequences': 2,
+    'no-shadow-restricted-names': 2,
+    'no-spaced-func': 2,
+    'no-sparse-arrays': 2,
+    'no-this-before-super': 2,
+    'no-throw-literal': 2,
+    'no-trailing-spaces': 2,
+    'no-undef': 2,
+    'no-undef-init': 2,
+    'no-unexpected-multiline': 2,
+    'no-unmodified-loop-condition': 2,
+    'no-unneeded-ternary': [2, {
+      'defaultAssignment': false
+    }],
+    'no-unreachable': 2,
+    'no-unsafe-finally': 2,
+    'no-unused-vars': [2, {
+      'vars': 'all',
+      'args': 'none'
+    }],
+    'no-useless-call': 2,
+    'no-useless-computed-key': 2,
+    'no-useless-constructor': 2,
+    'no-useless-escape': 0,
+    'no-whitespace-before-property': 2,
+    'no-with': 2,
+    'one-var': [2, {
+      'initialized': 'never'
+    }],
+    'operator-linebreak': [2, 'after', {
+      'overrides': {
+        '?': 'before',
+        ':': 'before'
+      }
+    }],
+    'padded-blocks': [2, 'never'],
+    'quotes': [2, 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true
+    }],
+    'semi': [2, 'never'],
+    'semi-spacing': [2, {
+      'before': false,
+      'after': true
+    }],
+    'space-before-blocks': [2, 'always'],
+    'space-before-function-paren': [2, 'never'],
+    'space-in-parens': [2, 'never'],
+    'space-infix-ops': 2,
+    'space-unary-ops': [2, {
+      'words': true,
+      'nonwords': false
+    }],
+    'spaced-comment': [2, 'always', {
+      'markers': ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
+    }],
+    'template-curly-spacing': [2, 'never'],
+    'use-isnan': 2,
+    'valid-typeof': 2,
+    'wrap-iife': [2, 'any'],
+    'yield-star-spacing': [2, 'both'],
+    'yoda': [2, 'never'],
+    'prefer-const': 2,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'object-curly-spacing': [2, 'always', {
+      objectsInObjects: false
+    }],
+    'array-bracket-spacing': [2, 'never']
+  }
 };
--- config-overrides.js
+++ config-overrides.js
@@ -1,29 +1,29 @@
-const { override, fixBabelImports,addDecoratorsLegacy, addWebpackAlias, addWebpackPlugin } = require('customize-cra');
-const path = require('path');
+const { override, fixBabelImports,addDecoratorsLegacy, addWebpackAlias, addWebpackPlugin } = require('customize-cra')
+const path = require('path')
 const chalk = require('chalk')
 const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 
-const resolve = dir => path.join(__dirname, dir);
+const resolve = dir => path.join(__dirname, dir)
 
 module.exports = override(
-  fixBabelImports('import', { // antd 按需加载 
+  fixBabelImports('import', { // antd 按需加载
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: true,
+    style: true
   }),
   addDecoratorsLegacy(), // 添加装饰器的能力
-  addWebpackAlias({  // 配置路径别名
-    '@': resolve('src'),
+  addWebpackAlias({ // 配置路径别名
+    '@': resolve('src')
   }),
   addWebpackPlugin(new ProgressBarPlugin({ // 加载进度条
     complete: '█',
     format: `${chalk.green('Building')} [ ${chalk.green(':bar')} ] ':msg:' ${chalk.bold('(:percent)')}`,
-    clear: true,
+    clear: true
   })),
-  config => {  // 关闭生产环境下打包生成map文件
+  config => { // 关闭生产环境下打包生成map文件
     if (process.env.NODE_ENV === 'production') {
-      config.devtool = false;
+      config.devtool = false
     }
-    return config;
-  },
-);
+    return config
+  }
+)
